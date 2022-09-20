99a229b0d63f5e95995936e16e380666d842cb6e
Stefan <arnautu.stefan93@gmail.com>
2022-09-20 12:10:11 +0000

    Delete src folder

--- src/hello_world/hello_world.cpp
+++
@@ -1,7 +0,0 @@
-#include "hello_world.hpp"
-
-namespace CMakeTest::HelloWorld {
-
-std::string fn_hello_world() { return "hello world\n"; }
-
-}  // namespace CMakeTest::HelloWorld
--- src/hello_world/hello_world.hpp
+++
@@ -1,7 +0,0 @@
-#include <string>
-
-namespace CMakeTest::HelloWorld {
-
-std::string fn_hello_world();
-
-}  // namespace CMakeTest::HelloWorld
